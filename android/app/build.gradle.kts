6480c229a9b458f98fb55317d3925adf6cd3296d
sritiz <sritizsahu07@gmail.com>
2026-08-29 18:18:20 +0530

    build: bump versionCode to 4 and versionName to 1.0.4 for Play Console release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "app.vaniagent.com"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = (flutter.versionCode ?: 4)
+        versionName = flutter.versionName ?: "1.0.4"
         ndk {
             abiFilters.addAll(listOf("arm64-v8a", "x86_64"))
         }
